861fc4330b43a8f6f7019d71187f8f763a57ce64
LENOVO <www.1411707895@qq.com>
2017-05-09 14:32:48 +0800

    Initial commit

---
+++ app/src/main/java/china/jiyun/com/os_chinatest/HomeActivity.java
@@ -0,0 +1,13 @@
+package china.jiyun.com.os_chinatest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/china/jiyun/com/os_chinatest/WelcomeActivity.java
@@ -0,0 +1,28 @@
+package china.jiyun.com.os_chinatest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initData();
+    }
+
+    private void initData() {
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        }, 2000);
+    }
+}
